119d03d57e9903cbb7e5353b563974d344291b89
im7mortal <im7mortal@gmail.com>
2016-03-25 15:21:17 +0300

    adminGroup schema

---
+++ schemas/adminGroup.go
@@ -0,0 +1,32 @@
+package schemas
+
+import (
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Permission struct {
+	Name string `bson:"name"`
+	Permit bool `bson:"permit"`
+}
+
+type AdminGroup struct {
+	ID          bson.ObjectId `bson:"_id,omitempty"`
+	Name        string `bson:"name"`
+	Permissions []Permission `bson:"permissions"`
+}
+
+func (u *AdminGroup) Flow()  {
+
+}
+
+
+
+var AdminGroupIndex mgo.Index = mgo.Index{
+	Key:        []string{"name"},
+	Unique:     true,
+	DropDups:   true,
+	Background: true,
+	Sparse:     true,
+	Name:       "adminGroupIndex",
+}
