46c9b88088c63d76e2b2e94fc64c7ad904ebb429
im7mortal <im7mortal@gmail.com>
2016-03-25 16:48:36 +0300

    first handler

---
+++ about.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Smoc1k(c *gin.Context) {
+
+	trigger = !trigger
+
+	/*	if trigger {
+
+			Router.SetHTMLTemplate(tm1)
+			println(tm1.DefinedTemplates())
+		} else {
+
+			Router.SetHTMLTemplate(tm2)
+			println(tm2.DefinedTemplates())
+		}*/
+	c.HTML(http.StatusOK, "default.html", gin.H{
+		"title": "Main website",
+		"pipeline": true,
+	})
+}
