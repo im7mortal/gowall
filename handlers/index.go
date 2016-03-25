1bca4c7b7fc99373dcc93bc72f60fef5c450ac72
im7mortal <im7mortal@gmail.com>
2016-03-25 16:59:37 +0300

    split handlers

---
+++ handlers/index.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Index(c *gin.Context) {
+	c.HTML(http.StatusOK, "default.html", gin.H{
+		"title": "Main website",
+		"pipeline": true,
+	})
+}
--- routes.go
+++ routes.go
@@ -1,153 +1,156 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/im7mortal/gowall/handlers"
+)
 
 func BindRoutes(router *gin.Engine) {
 
 	//front end
-	//router.GET("/", Smock)
-	router.GET("/about/", About)
-	router.GET("/contact/", About)
-	router.POST("/contact/", About)
+	router.GET("/", handlers.Index)
+	router.GET("/about/", handlers.Index)
+	router.GET("/contact/", handlers.Index)
+	router.POST("/contact/", handlers.Index)
 
 	//sign up
-	router.GET("/signup/", About)
-	router.POST("/signup/", About)
+	router.GET("/signup/", handlers.Index)
+	router.POST("/signup/", handlers.Index)
 
 	//social sign up
-	router.POST("/signup/social/", About)
-	router.GET("/signup/twitter/", About)
-	router.GET("/signup/twitter/callback/", About)
-	router.GET("/signup/github/", About)
-	router.GET("/signup/github/callback/", About)
-	router.GET("/signup/facebook/", About)
-	router.GET("/signup/facebook/callback/", About)
-	router.GET("/signup/google/", About)
-	router.GET("/signup/google/callback/", About)
-	router.GET("/signup/tumblr/", About)
-	router.GET("/signup/tumblr/callback/", About)
+	router.POST("/signup/social/", handlers.Index)
+	router.GET("/signup/twitter/", handlers.Index)
+	router.GET("/signup/twitter/callback/", handlers.Index)
+	router.GET("/signup/github/", handlers.Index)
+	router.GET("/signup/github/callback/", handlers.Index)
+	router.GET("/signup/facebook/", handlers.Index)
+	router.GET("/signup/facebook/callback/", handlers.Index)
+	router.GET("/signup/google/", handlers.Index)
+	router.GET("/signup/google/callback/", handlers.Index)
+	router.GET("/signup/tumblr/", handlers.Index)
+	router.GET("/signup/tumblr/callback/", handlers.Index)
 
 	//login/out
-	router.GET("/login/", About)
-	router.POST("/login/", About)
-	router.GET("/login/forgot/", About)
-	router.POST("/login/forgot/", About)
-	router.GET("/login/reset/", About)
-	router.GET("/login/reset/:email/:token/", About)
-	router.PUT("/login/reset/:email/:token/", About)
-	router.GET("/logout/", About)
+	router.GET("/login/", handlers.Index)
+	router.POST("/login/", handlers.Index)
+	router.GET("/login/forgot/", handlers.Index)
+	router.POST("/login/forgot/", handlers.Index)
+	router.GET("/login/reset/", handlers.Index)
+	router.GET("/login/reset/:email/:token/", handlers.Index)
+	router.PUT("/login/reset/:email/:token/", handlers.Index)
+	router.GET("/logout/", handlers.Index)
 
 	//social login
-	router.GET("/login/twitter/", About)
-	router.GET("/login/twitter/callback/", About)
-	router.GET("/login/github/", About)
-	router.GET("/login/github/callback/", About)
-	router.GET("/login/facebook/", About)
-	router.GET("/login/facebook/callback/", About)
-	router.GET("/login/google/", About)
-	router.GET("/login/google/callback/", About)
-	router.GET("/login/tumblr/", About)
-	router.GET("/login/tumblr/callback/", About)
+	router.GET("/login/twitter/", handlers.Index)
+	router.GET("/login/twitter/callback/", handlers.Index)
+	router.GET("/login/github/", handlers.Index)
+	router.GET("/login/github/callback/", handlers.Index)
+	router.GET("/login/facebook/", handlers.Index)
+	router.GET("/login/facebook/callback/", handlers.Index)
+	router.GET("/login/google/", handlers.Index)
+	router.GET("/login/google/callback/", handlers.Index)
+	router.GET("/login/tumblr/", handlers.Index)
+	router.GET("/login/tumblr/callback/", handlers.Index)
 
 	//admin
 	//app.all('/admin*', ensureAuthenticated); TODO necessary midleware
 	//app.all('/admin*', ensureAdmin);
-	router.GET("/admin/", About)
+	router.GET("/admin/", handlers.Index)
 
 	//admin > users
-	router.GET("/admin/users/", About)
-	router.POST("/admin/users/", About)
-	router.GET("/admin/users/:id/", About)
-	router.PUT("/admin/users/:id/", About)
-	router.PUT("/admin/users/:id/password/", About)
-	router.PUT("/admin/users/:id/role-admin/", About)
-	router.DELETE("/admin/users/:id/role-admin/", About)
-	router.PUT("/admin/users/:id/role-account/", About)
-	router.DELETE("/admin/users/:id/role-account/", About)
-	router.DELETE("/admin/users/:id/", About)
+	router.GET("/admin/users/", handlers.Index)
+	router.POST("/admin/users/", handlers.Index)
+	router.GET("/admin/users/:id/", handlers.Index)
+	router.PUT("/admin/users/:id/", handlers.Index)
+	router.PUT("/admin/users/:id/password/", handlers.Index)
+	router.PUT("/admin/users/:id/role-admin/", handlers.Index)
+	router.DELETE("/admin/users/:id/role-admin/", handlers.Index)
+	router.PUT("/admin/users/:id/role-account/", handlers.Index)
+	router.DELETE("/admin/users/:id/role-account/", handlers.Index)
+	router.DELETE("/admin/users/:id/", handlers.Index)
 
 	//admin > administrators
-	router.GET("/admin/administrators/", About)
-	router.POST("/admin/administrators/", About)
-	router.GET("/admin/administrators/:id/", About)
-	router.PUT("/admin/administrators/:id/", About)
-	router.PUT("/admin/administrators/:id/permissions/", About)
-	router.PUT("/admin/administrators/:id/groups/", About)
-	router.PUT("/admin/administrators/:id/user/", About)
-	router.DELETE("/admin/administrators/:id/user/", About)
-	router.DELETE("/admin/administrators/:id/", About)
+	router.GET("/admin/administrators/", handlers.Index)
+	router.POST("/admin/administrators/", handlers.Index)
+	router.GET("/admin/administrators/:id/", handlers.Index)
+	router.PUT("/admin/administrators/:id/", handlers.Index)
+	router.PUT("/admin/administrators/:id/permissions/", handlers.Index)
+	router.PUT("/admin/administrators/:id/groups/", handlers.Index)
+	router.PUT("/admin/administrators/:id/user/", handlers.Index)
+	router.DELETE("/admin/administrators/:id/user/", handlers.Index)
+	router.DELETE("/admin/administrators/:id/", handlers.Index)
 
 	//admin > admin groups
-	router.GET("/admin/admin-groups/", About)
-	router.POST("/admin/admin-groups/", About)
-	router.GET("/admin/admin-groups/:id/", About)
-	router.PUT("/admin/admin-groups/:id/", About)
-	router.PUT("/admin/admin-groups/:id/permissions/", About)
-	router.DELETE("/admin/admin-groups/:id/", About)
+	router.GET("/admin/admin-groups/", handlers.Index)
+	router.POST("/admin/admin-groups/", handlers.Index)
+	router.GET("/admin/admin-groups/:id/", handlers.Index)
+	router.PUT("/admin/admin-groups/:id/", handlers.Index)
+	router.PUT("/admin/admin-groups/:id/permissions/", handlers.Index)
+	router.DELETE("/admin/admin-groups/:id/", handlers.Index)
 
 	//admin > accounts
-	router.GET("/admin/accounts/", About)
-	router.POST("/admin/accounts/", About)
-	router.GET("/admin/accounts/:id/", About)
-	router.PUT("/admin/accounts/:id/", About)
-	router.PUT("/admin/accounts/:id/user/", About)
-	router.DELETE("/admin/accounts/:id/user/", About)
-	router.POST("/admin/accounts/:id/notes/", About)
-	router.POST("/admin/accounts/:id/status/", About)
-	router.DELETE("/admin/accounts/:id/", About)
+	router.GET("/admin/accounts/", handlers.Index)
+	router.POST("/admin/accounts/", handlers.Index)
+	router.GET("/admin/accounts/:id/", handlers.Index)
+	router.PUT("/admin/accounts/:id/", handlers.Index)
+	router.PUT("/admin/accounts/:id/user/", handlers.Index)
+	router.DELETE("/admin/accounts/:id/user/", handlers.Index)
+	router.POST("/admin/accounts/:id/notes/", handlers.Index)
+	router.POST("/admin/accounts/:id/status/", handlers.Index)
+	router.DELETE("/admin/accounts/:id/", handlers.Index)
 
 
 	//admin > statuses
-	router.GET("/admin/statuses/", About)
-	router.POST("/admin/statuses/", About)
-	router.GET("/admin/statuses/:id/", About)
-	router.PUT("/admin/statuses/:id/", About)
-	router.DELETE("/admin/statuses/:id/", About)
+	router.GET("/admin/statuses/", handlers.Index)
+	router.POST("/admin/statuses/", handlers.Index)
+	router.GET("/admin/statuses/:id/", handlers.Index)
+	router.PUT("/admin/statuses/:id/", handlers.Index)
+	router.DELETE("/admin/statuses/:id/", handlers.Index)
 
 
 	//admin > categories
-	router.GET("/admin/categories/", About)
-	router.POST("/admin/categories/", About)
-	router.GET("/admin/categories/:id/", About)
-	router.PUT("/admin/categories/:id/", About)
-	router.DELETE("/admin/categories/:id/", About)
+	router.GET("/admin/categories/", handlers.Index)
+	router.POST("/admin/categories/", handlers.Index)
+	router.GET("/admin/categories/:id/", handlers.Index)
+	router.PUT("/admin/categories/:id/", handlers.Index)
+	router.DELETE("/admin/categories/:id/", handlers.Index)
 
 	//admin > search
-	router.GET("/admin/search/", About)
+	router.GET("/admin/search/", handlers.Index)
 
 	//account
 	//app.all('/account*', ensureAuthenticated); //TODO necessary midleware
 	//app.all('/account*', ensureAccount);
-	router.GET("/account/", About)
+	router.GET("/account/", handlers.Index)
 
 	//account > verification
-	router.GET("/account/verification/", About)
-	router.POST("/account/verification/", About)
-	router.GET("/account/verification/:token/", About)
+	router.GET("/account/verification/", handlers.Index)
+	router.POST("/account/verification/", handlers.Index)
+	router.GET("/account/verification/:token/", handlers.Index)
 
 	//account > settings
-	router.GET("/account/settings/", About)
-	router.PUT("/account/settings/", About)
-	router.PUT("/account/settings/identity/", About)
-	router.PUT("/account/settings/password/", About)
+	router.GET("/account/settings/", handlers.Index)
+	router.PUT("/account/settings/", handlers.Index)
+	router.PUT("/account/settings/identity/", handlers.Index)
+	router.PUT("/account/settings/password/", handlers.Index)
 
 	//account > settings > social
-	router.GET("/account/settings/twitter/", About)
-	router.GET("/account/settings/twitter/callback/", About)
-	router.GET("/account/settings/twitter/disconnect/", About)
-	router.GET("/account/settings/github/", About)
-	router.GET("/account/settings/github/callback/", About)
-	router.GET("/account/settings/github/disconnect/", About)
-	router.GET("/account/settings/facebook/", About)
-	router.GET("/account/settings/facebook/callback/", About)
-	router.GET("/account/settings/facebook/disconnect/", About)
-	router.GET("/account/settings/google/", About)
-	router.GET("/account/settings/google/callback/", About)
-	router.GET("/account/settings/google/disconnect/", About)
-	router.GET("/account/settings/tumblr/", About)
-	router.GET("/account/settings/tumblr/callback/", About)
-	router.GET("/account/settings/tumblr/disconnect/", About)
+	router.GET("/account/settings/twitter/", handlers.Index)
+	router.GET("/account/settings/twitter/callback/", handlers.Index)
+	router.GET("/account/settings/twitter/disconnect/", handlers.Index)
+	router.GET("/account/settings/github/", handlers.Index)
+	router.GET("/account/settings/github/callback/", handlers.Index)
+	router.GET("/account/settings/github/disconnect/", handlers.Index)
+	router.GET("/account/settings/facebook/", handlers.Index)
+	router.GET("/account/settings/facebook/callback/", handlers.Index)
+	router.GET("/account/settings/facebook/disconnect/", handlers.Index)
+	router.GET("/account/settings/google/", handlers.Index)
+	router.GET("/account/settings/google/callback/", handlers.Index)
+	router.GET("/account/settings/google/disconnect/", handlers.Index)
+	router.GET("/account/settings/tumblr/", handlers.Index)
+	router.GET("/account/settings/tumblr/callback/", handlers.Index)
+	router.GET("/account/settings/tumblr/disconnect/", handlers.Index)
 
 	//route not found
-	router.NoRoute(About)
+	router.NoRoute(handlers.Index)
 }
