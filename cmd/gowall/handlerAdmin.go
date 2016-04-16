a356696cbe54e5ed96e0a7adabe93a5a21b44b74
im7mortal <im7mortal@gmail.com>
2016-04-17 02:07:55 +0300

    [admin] render

--- cmd/gowall/handlerAdmin.go
+++ cmd/gowall/handlerAdmin.go
@@ -29,7 +29,7 @@ func generateToken1(n int) []byte {
 func AdminRender(c *gin.Context) {
 	render, _ := TemplateStorage[c.Request.URL.Path]
 
-	// TODO
+/*	// TODO
 	db := getMongoDBInstance()
 	defer db.Session.Close()
 	collection := db.C(ACCOUNTS)
@@ -49,7 +49,31 @@ func AdminRender(c *gin.Context) {
 	c.Set("CountCategory", count)
 	collection = db.C(STATUS)
 	count, _ = collection.Count()
-	c.Set("CountStatus", count)
+	c.Set("CountStatus", count)*/
+
+	db := getMongoDBInstance()
+	defer db.Session.Close()
+
+	CountAccountChan := make(chan int)
+	CountUserChan := make(chan int)
+	CountAdminChan := make(chan int)
+	CountAdminGroupChan := make(chan int)
+	CountCategoryChan := make(chan int)
+	CountStatusChan := make(chan int)
+
+	go getCount(db.C(ACCOUNTS), CountAccountChan, bson.M{})
+	go getCount(db.C(USERS), CountUserChan, bson.M{})
+	go getCount(db.C(ADMINS), CountAdminChan, bson.M{})
+	go getCount(db.C(ADMINGROUPS), CountAdminGroupChan, bson.M{})
+	go getCount(db.C(CATEGORIES), CountCategoryChan, bson.M{})
+	go getCount(db.C(STATUS), CountStatusChan, bson.M{})
+
+	c.Set("CountAccount", <- CountAccountChan)
+	c.Set("CountUser", <- CountUserChan)
+	c.Set("CountAdmin", <- CountAdminChan)
+	c.Set("CountAdminGroup", <- CountAdminGroupChan)
+	c.Set("CountCategory", <- CountCategoryChan)
+	c.Set("CountStatus", <- CountStatusChan)
 
 	render.Data = c.Keys
 	c.Render(http.StatusOK, render)
