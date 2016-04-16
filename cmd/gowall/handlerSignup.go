94ff18bf25daa0ee8874fc43ce098e36dca222a2
im7mortal <im7mortal@gmail.com>
2016-04-16 23:35:39 +0300

    [refactoring] signUp

--- cmd/gowall/handlerSignup.go
+++ cmd/gowall/handlerSignup.go
@@ -3,23 +3,12 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"regexp"
 	"gopkg.in/mgo.v2/bson"
 	"strings"
 	"encoding/json"
 	"golang.org/x/crypto/bcrypt"
 	"github.com/gin-gonic/contrib/sessions"
-	"github.com/markbates/goth/gothic"
-	"github.com/markbates/goth"
-	"github.com/markbates/goth/providers/facebook"
 )
-
-
-func init()  {
-	gothic.Store = store
-}
-
-
 
 func SignupRender(c *gin.Context) {
 	isAuthenticated, _ := c.Get("isAuthenticated")
@@ -49,48 +38,23 @@ func SignupRender(c *gin.Context) {
 
 func Signup(c *gin.Context) {
 	response := Response{} // todo sync.Pool
-	response.Errors = []string{}
-	response.ErrFor = make(map[string]string)
 	defer response.Recover(c)
 
-	var body struct {
-		Username    string  `json:"username"`
-		Email   string  `json:"email"`
-		Password string  `json:"password"`
-	}
 	decoder := json.NewDecoder(c.Request.Body)
-	err := decoder.Decode(&body)
+	err := decoder.Decode(&response)
+	if err != nil {
+		response.Errors = append(response.Errors, err.Error())
+		response.Fail(c)
+		return
+	}
+	// clean errors from client
+	response.CleanErrors()
 
+	// validate
+	response.ValidateUsername()
+	response.ValidateEmail()
+	response.ValidatePassword()
 
-	username := strings.ToLower(body.Username)
-	if len(username) == 0 {
-		response.ErrFor["username"] = "required"
-	} else {
-		r, err := regexp.MatchString(`^[a-zA-Z0-9\-\_]+$`, username)
-		if err != nil {
-			println(err.Error())
-		}
-		if !r {
-			response.ErrFor["username"] = `only use letters, numbers, \'-\', \'_\'`
-		}
-	}
-	email := strings.ToLower(body.Email)
-	if len(email) == 0 {
-		response.ErrFor["email"] = "required"
-	} else {
-		r, err := regexp.MatchString(`^[a-zA-Z0-9\-\_\.\+]+@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z0-9\-\_]+$`, email)
-		if err != nil {
-			println(err.Error())
-		}
-		if !r {
-			response.ErrFor["email"] = `invalid email format`
-		}
-	}
-	password := body.Password
-	//destruct body??? todo
-	if len(password) == 0 {
-		response.ErrFor["password"] = "required"
-	}
 	if response.HasErrors() {
 		response.Fail(c)
 		return
@@ -99,16 +63,19 @@ func Signup(c *gin.Context) {
 	db := getMongoDBInstance()
 	defer db.Session.Close()
 	collection := db.C(USERS)
-	us := User{}
-	err = collection.Find(bson.M{"$or": []bson.M{bson.M{"username": username}, bson.M{"email": email}}}).One(&us)
+	user := User{}
+	err = collection.Find(bson.M{"$or": []bson.M{bson.M{"username": response.Username}, bson.M{"email": response.Email}}}).One(&user)
 	if err != nil {
 		println(err.Error())
 	}
-	if len(us.Username) != 0 {
-		if us.Username == username {
+
+	// duplicateUsernameCheck
+	// duplicateEmailCheck
+	if len(user.Username) != 0 {
+		if user.Username == response.Username {
 			response.ErrFor["username"] = "username already taken"
 		}
-		if us.Email == email {
+		if user.Email == response.Email {
 			response.ErrFor["email"] = "email already registered"
 		}
 	}
@@ -117,235 +84,36 @@ func Signup(c *gin.Context) {
 		return
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	// createUser
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(response.Password), bcrypt.DefaultCost)
 	if err != nil {
 		response.Errors = append(response.Errors, err.Error())
 		response.Fail(c)
 		return
 	}
 
-	us.ID = bson.NewObjectId()
-	us.IsActive = "yes"
-	us.Username = username
-	us.Email = strings.ToLower(email)
-	us.Password = string(hashedPassword)
-	us.Search = []string{username, email}
+	user.ID = bson.NewObjectId()
+	user.IsActive = "yes"
+	user.Username = response.Username
+	user.Email = strings.ToLower(response.Email)
+	user.Password = string(hashedPassword)
+	user.Search = []string{response.Username, response.Email}
 
-	err = collection.Insert(us)
+	err = collection.Insert(user)
 	if err != nil {
 		response.Errors = append(response.Errors, err.Error())
 		response.Fail(c)
 		return
 	}
 
-	ac := Account{}
+	// createAccount
+	account := Account{}
 
-	ac.ID = bson.NewObjectId()
+	account.ID = bson.NewObjectId()
 
-	us.Roles.Account = ac.ID
-
-	err = collection.UpdateId(us.ID, us)
-	if err != nil {
-		response.Errors = append(response.Errors, err.Error())
-		response.Fail(c)
-		return
-	}
-
-	if config.RequireAccountVerification {
-		ac.IsVerified = "no"
-	} else {
-		ac.IsVerified = "yes"
-	}
-	ac.Name.Full = username
-	ac.User.ID = us.ID
-	ac.User.Name = us.Username
-	ac.Search = []string{username}
-
-	collection = db.C(ACCOUNTS)
-	err = collection.Insert(ac)
-	if err != nil {
-		response.Errors = append(response.Errors, err.Error())
-		response.Fail(c)
-		return
-	}
-
-	//todo  sendWelcomeEmail
-	//todo  sendWelcomeEmail ***************************************************
-	//put in the c.Keys
-	c.Set("Username", username)
-	c.Set("Email", email)
-	c.Set("LoginURL", "http://" + c.Request.Host + "/login/")
-
-	mailConf := MailConfig{}
-	mailConf.Data = c.Keys
-	mailConf.From = config.SMTP.From.Name + " <" + config.SMTP.From.Address + ">"
-	mailConf.To = config.SystemEmail
-	mailConf.Subject = "Your " + config.ProjectName + " Account"
-	mailConf.ReplyTo = body.Email
-	mailConf.HtmlPath = "views/signup/email-html.html"
-
-	if err := mailConf.SendMail(); err != nil {
-		//todo it's not serious
-	}
-
-	sess := sessions.Default(c)
-	sess.Set("public", us.ID.Hex())
-	sess.Save()
-
-	response.Success = true
-	c.JSON(http.StatusOK, response)
-}
-
-func startOAuth(c *gin.Context) {
-	// don't like that hack
-	// gothic was written for another path
-	// i just put provider query
-	provider := c.Param("provider")
-	c.Request.URL.RawQuery += "provider=" + provider
-	_, err := goth.GetProvider(provider)
-	if err != nil {
-		redir := "http://" + c.Request.Host + "/signup_/facebook/callback"
-		goth.UseProviders(
-			facebook.New(config.Socials["facebook"].Key, config.Socials["facebook"].Secret, redir),
-		)
-		println(")))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))")
-		println(redir)
-		println(")))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))")
-	}
-	gothic.BeginAuthHandler(c.Writer, c.Request)
-}
-
-func CompleteUserAuth(c *gin.Context) {
-	// gothic was written for another path
-	// i just put provider query
-	c.Request.URL.RawQuery += "&provider=" + c.Param("provider")
-	// print our state string to the console. Ideally, you should verify
-	// that it's the same string as the one you set in `setState`
-	user_, err := gothic.CompleteUserAuth(c.Writer, c.Request)
-	if err != nil {
-		println(err.Error())
-		//todo error handling
-		return
-	}
-	if len(user_.Name) == 0 {
-		println("12  error")
-		//todo error handling
-		return
-	}
-	user_string, _ := json.Marshal(user_)
-
-	sessionCookie := sessions.Default(c)
-	sessionCookie.Set("socialProfile", user_string)
-	sessionCookie.Set("provider", c.Param("provider"))
-	sessionCookie.Save()
-
-	if len(user_.Email) == 0 {
-		render, _ := TemplateStorage["/signup/social/"]
-		render.Data = c.Keys
-		c.Render(http.StatusOK, render)
-		return
-	}
-
-
-	SignUpSocial(c)
-}
-
-func SignUpSocial(c *gin.Context) {
-
-	response := Response{} // todo sync.Pool
-	response.Errors = []string{}
-	response.ErrFor = make(map[string]string)
-	defer response.Recover(c)
-
-	var body struct {
-		Email   string  `json:"email"`
-	}
-	decoder := json.NewDecoder(c.Request.Body)
-	err := decoder.Decode(&body)
-
-	email := strings.ToLower(body.Email)
-	if len(email) == 0 {
-		response.ErrFor["email"] = "required"
-	} else {
-		r, err := regexp.MatchString(`^[a-zA-Z0-9\-\_\.\+]+@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z0-9\-\_]+$`, email)
-		if err != nil {
-			println(err.Error())
-		}
-		if !r {
-			response.ErrFor["email"] = `invalid email format`
-		}
-	}
-
-	if response.HasErrors() {
-		response.Fail(c)
-		return
-	}
-	session := sessions.Default(c)
-
-	socialProfile_, ok := session.Get("socialProfile").(string)
-	if ok && len(socialProfile_) == 0 {
-		return
-	}
-	socialProfile := goth.User{}
-	err = json.Unmarshal([]byte(socialProfile_), &socialProfile)
-	if err != nil {
-
-	}
-
-	var username string
-	if len(socialProfile.Name) != 0 {
-		username = socialProfile.Name
-	} else if len(socialProfile.UserID) != 0 {
-		username = socialProfile.UserID
-	}
-	reg, err := regexp.Compile(`/[^a-zA-Z0-9\-\_]/g`)
-
-
-	usernameSrc := []byte(username)
-	reg.ReplaceAll(usernameSrc, []byte(""))
-	username = string(usernameSrc)
-
-
-	db := getMongoDBInstance()
-	defer db.Session.Close()
-	collection := db.C(USERS)
-	us := User{} // todo pool
-	err = collection.Find(bson.M{"$or": []bson.M{bson.M{"username": username}, bson.M{"email": email}}}).One(&us)
-
-	if len(us.Username) != 0 {
-		response.Fail(c)
-		return
-	}
-	if len(us.Email) != 0 {
-		response.Fail(c)
-		return
-	}
-
-	us.ID = bson.NewObjectId()
-	us.IsActive = "yes"
-	us.Username = us.ID.Hex()
-	us.Email = strings.ToLower(email)
-	us.Search = []string{username, email}
-
-	//provider, _ := session.Get("provider").(string)
-
-	us.Facebook = vendorOauth{}
-	us.Facebook.ID = socialProfile.UserID
-
-	err = collection.Insert(us)
-
-	if err != nil {
-		println(err.Error())
-		//todo error handling
-		return
-	}
-	ac := Account{}
-
-	ac.ID = bson.NewObjectId()
-
-	us.Roles.Account = ac.ID
-
-	err = collection.UpdateId(us.ID, us)
+	//update user with account
+	user.Roles.Account = account.ID
+	err = collection.UpdateId(user.ID, user)
 	if err != nil {
 		response.Errors = append(response.Errors, err.Error())
 		response.Fail(c)
@@ -353,26 +121,51 @@ func SignUpSocial(c *gin.Context) {
 	}
 
 	if config.RequireAccountVerification {
-		ac.IsVerified = "no"
+		account.IsVerified = "no"
 	} else {
-		ac.IsVerified = "yes"
+		account.IsVerified = "yes"
 	}
-	ac.Name.Full = username
-	ac.User.ID = us.ID
-	ac.User.Name = us.Username
-	ac.Search = []string{username}
+	account.Name.Full = response.Username
+	account.User.ID = user.ID
+	account.User.Name = user.Username
+	account.Search = []string{response.Username}
 
 	collection = db.C(ACCOUNTS)
-	err = collection.Insert(ac)
+	err = collection.Insert(account)
 	if err != nil {
 		response.Errors = append(response.Errors, err.Error())
 		response.Fail(c)
 		return
 	}
-	sess := sessions.Default(c)
-	sess.Set("public", us.ID.Hex())
-	sess.Save()
+
+	// sendWelcomeEmail
+	go func() {
+		c.Set("Username", response.Username)
+		c.Set("Email", response.Email)
+		c.Set("LoginURL", "http://" + c.Request.Host + "/login/")
+
+		mailConf := MailConfig{}
+		mailConf.Data = c.Keys
+		mailConf.From = config.SMTP.From.Name + " <" + config.SMTP.From.Address + ">"
+		mailConf.To = config.SystemEmail
+		mailConf.Subject = "Your " + config.ProjectName + " Account"
+		mailConf.ReplyTo = response.Email
+		mailConf.HtmlPath = "views/signup/email-html.html"
+
+		if err := mailConf.SendMail(); err != nil {
+			println("Error Sending Welcome Email: " + err.Error())
+		}
+	}()
+
+	// logUserIn
+	user.login(c)
 
 	response.Success = true
 	c.JSON(http.StatusOK, response)
+}
+
+func (user *User)login(c *gin.Context) {
+	sess := sessions.Default(c)
+	sess.Set("public", user.ID.Hex())
+	sess.Save()
 }
--- cmd/gowall/workflow.go
+++ cmd/gowall/workflow.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"github.com/gin-gonic/gin/render"
 	"html/template"
+	"strings"
+	"regexp"
 )
 
 var TemplateStorage map[string]*render.HTML = make(map[string]*render.HTML)
@@ -22,6 +24,10 @@ type Response struct {
 	Success bool `json:"success"`
 	Errors  []string `json:"errors"`
 	ErrFor  map[string]string `json:"errfor"`
+
+	Username    string  `json:"username"`
+	Email   string  `json:"email"`
+	Password string  `json:"password"`
 }
 
 func (r *Response)HasErrors() bool {
@@ -34,3 +40,48 @@ func (r *Response)Fail(c *gin.Context) {
 }
 
 func (r *Response) Recover(c *gin.Context) {}
+
+func (r *Response) ValidateUsername() {
+	r.Username = strings.ToLower(r.Username)
+	if len(r.Username) == 0 {
+		r.ErrFor["username"] = "required"
+	} else {
+		ok, err := regexp.MatchString(`^[a-zA-Z0-9\-\_]+$`, r.Username)
+		if err != nil {
+			println(err.Error())
+		}
+		if !ok {
+			r.ErrFor["username"] = `only use letters, numbers, \'-\', \'_\'`
+		}
+	}
+}
+
+func (r *Response) ValidateEmail() {
+	r.Email = strings.ToLower(r.Email)
+	if len(r.Email) == 0 {
+		r.ErrFor["email"] = "required"
+	} else {
+		ok, err := regexp.MatchString(`^[a-zA-Z0-9\-\_\.\+]+@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z0-9\-\_]+$`, r.Email)
+		if err != nil {
+			println(err.Error())
+		}
+		if !ok {
+			r.ErrFor["email"] = `invalid email format`
+		}
+	}
+}
+
+func (r *Response) ValidatePassword() {
+	if len(r.Password) == 0 {
+		r.ErrFor["password"] = "required"
+	} else {
+		if len(r.Password) < 8 {
+			r.ErrFor["password"] = `too weak password`
+		}
+	}
+}
+
+func (r *Response) CleanErrors() {
+	r.Errors = []string{}
+	r.ErrFor = make(map[string]string)
+}
