5b7f3c44e2dfaa44824c8bfa77362d189c1bee37
im7mortal <im7mortal@gmail.com>
2016-04-27 15:32:24 +0300

    [admin] works somehow

--- cmd/gowall/handlerAdminUsers.go
+++ cmd/gowall/handlerAdminUsers.go
@@ -38,46 +38,70 @@ func AdminUsersRender(c *gin.Context) {
 		}
 	}
 
-	limit_ := c.DefaultQuery("limit", "20")
-	limit, _ := strconv.ParseInt(limit_, 0, 0)
+	limitS := c.DefaultQuery("limit", "20")
+	limit_, _ := strconv.ParseInt(limitS, 0, 0)
+	limit := int(limit_)
 	if limit > 100 {
 		limit = 100
 	}
 
-	page_ := c.DefaultQuery("page", "0")
-	page, _ := strconv.ParseInt(page_, 0, 0)
-
+	pageS := c.DefaultQuery("page", "0")
+	page_, _ := strconv.ParseInt(pageS, 0, 0)
+	page := int(page_)
 	sort := c.DefaultQuery("sort", "_id")
 
-	var results []User
+	type _user struct {
+		ID string `bson:"_id" json:"_id"`
+		Username string `bson:"username" json:"username"`
+		IsActive string `bson:"isActive" json:"isActive"`
+		Email string `bson:"email" json:"email"`
+	}
+	var results []_user
 
 	db := getMongoDBInstance()
 	defer db.Session.Close()
+	// TODO keys
 	collection := db.C(USERS)
+	_query := collection.Find(query) // TODO 2 query
+	count, _ := _query.Count()
+	_query.Skip(page * limit).Sort(sort).Limit(limit).All(&results)
 
-	collection.Find(query).Skip(int(limit * page)).Sort(sort).Limit(int(limit)).All(&results)
-
-	users := []gin.H{}
-
-	for _, user := range results {
+	//users := []gin.H{}
+/*	for _, user := range results {
 		users = append(users, gin.H{
 			"_id": user.ID.Hex(),
 			"username": user.Username,
 			"isActive": user.IsActive,
 			"email": user.Email,
 		})
+	}*/
+
+	page += 1
+	count_ := page * limit
+	pages := gin.H{
+		"current": page,
+		"prev": page - 1,
+		"hasPrev": page - 1 != 0,
+		"next": page + 1,
+		"hasNext": float64(count) / float64(count_) > 1,
+		"total": count,
+	}
+
+	end := count_
+	if count_ > count {
+		end = count
+	}
+
+	items := gin.H{
+		"begin": (page - 1) * limit,
+		"end": end,
+		"total": count,
 	}
 
 	Result := gin.H{
-		"data": users,
-		"pages": gin.H{
-			"current": page + 1,
-			"prev": 0,
-			"hasPrev": false,
-			"next": 0,
-			"hasNext": false,
-			"total": 0,
-		},
+		"data": results,
+		"pages": pages,
+		"items": items,
 	}
 
 	Results, _ := json.Marshal(Result)
@@ -112,3 +136,4 @@ func UsersRender(c *gin.Context) {
 func XHR(c *gin.Context) bool {
 	return strings.ToLower(c.Request.Header.Get("X-Requested-With")) == "xmlhttprequest"
 }
+
